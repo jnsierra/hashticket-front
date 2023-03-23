2dd8dd642d390345a0984321eb68ea02103f0642
Jesus Nicolas Sierra Chaparro <nicolas@iMac-de-Jesus.local>
2023-03-23 16:19:39 -0500

    Despliegue por medio de docker

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     urlBaseBussines : '/api/api-business/',
     urlBaseDatos: '/api/api-acceso-datos/',
-    urlPublic: '/api/api-public-users/'
+    urlBasePublic: '/api/api-public-users/'
 };
